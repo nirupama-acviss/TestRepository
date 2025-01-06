bc68ec665b7b90d5a2bc980d0f29ad02be67ca9e
Prabhu <prabhu.m@acviss.com>
2025-01-06 16:57:11 +0530

    update

--- src/test/java/api/endpoints/Routes.java
+++ src/test/java/api/endpoints/Routes.java
@@ -3,5 +3,10 @@ package api.endpoints;
 public class Routes {
 	
 	public static String baseurl="https://lamina.test.acviss.co/api/v2";
+	
+	private void Sysout() {
+		// TODO Auto-generated method stub
+System.out.println("new prabhu chANGES");
+	}
 
 }
